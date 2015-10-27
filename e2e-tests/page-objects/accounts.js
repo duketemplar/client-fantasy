c622bbc2368ae213ed211e8d5101c02a49a1820d
Andreas Ronge <andreas.ronge@gmail.com>
2015-10-27 12:19:41 +0100

    Fixed page object

--- e2e-tests/page-objects/accounts.js
+++ e2e-tests/page-objects/accounts.js
@@ -1,6 +1,36 @@
+const devMode = process.env.NIGHTWATCH_ENV === 'dev';
+
 function accountUrl(client) {
-  const rootPage = (process.env.NIGHTWATCH_ENV === 'dev') ? '' : '/mux/web/nordnet/seed.html';
+  // if you want to debug production environment from your local machine:
+  // return 'https://nordnet.webfront1.ci.nordnet.se/mux/web/nordnet/seed.html';
+  const rootPage = devMode ? '' : '/mux/web/nordnet/seed.html';
   return client.launch_url + rootPage;
+}
+
+function loginDevMode(client) {
+  return (username = 'stora', password = 'not needed', locale = 'sv-SE') => {
+    console.log(`loginDevMode username: ${username} password: ${password}`);
+    client.waitForElementVisible('select', 1000);
+    client.waitForElementVisible('input', 1000);
+    client.expect.element('#login-container').to.be.present;
+    client.setValue('input[type=text]', username);
+    client.expect.element('button').to.be.present;
+    client.click(`select option[value="${locale}"]`);
+    client.waitForElementVisible('select', 1000);
+    client.click('button');
+  };
+}
+
+function loginProdMode(client) {
+  return (username = 'automat-se-1', password = 'Xd42pafb') => {
+    console.log(`loginProdMode username: ${username} password: ${password}`);
+    client.expect.element('#supportNavLogin').to.be.present;
+    client.click('#supportNavLogin a');
+    client.waitForElementVisible('form#loginForm', 5000);
+    client.setValue('input#input1', username);
+    client.setValue('input#pContent', password);
+    client.click('button#login_btn');
+  };
 }
 
 export default (client) => {
@@ -10,20 +40,9 @@ export default (client) => {
     url,
     gotoAccounts() {
       client.url(url);
-      client.waitForElementVisible('body', 1000);
-      client.expect.element('#nordnet-react-app-light').to.be.present;
+      client.waitForElementVisible('body', 10000);
     },
 
-    // locale - e.g. sv-SE
-    login(username, password, locale) {
-      client.waitForElementVisible('select', 1000);
-      client.waitForElementVisible('input', 1000);
-      client.expect.element('#login-container').to.be.present;
-      client.setValue('input[type=text]', username);
-      client.expect.element('button').to.be.present;
-      client.click(`select option[value="${locale}"]`);
-      client.waitForElementVisible('select', 1000);
-      client.click('button');
-    },
+    login: (devMode) ? loginDevMode(client) : loginProdMode(client),
   };
 }
--- e2e-tests/tests/smoke.test.js
+++ e2e-tests/tests/smoke.test.js
@@ -1,56 +1,42 @@
-console.log('ENV', process.env.NIGHTWATCH_ENV);
-
-const url = ('dev' === process.env.NIGHTWATCH_ENV) ? '' : '/mux/web/nordnet/seed.html';
-
-// How to debug:
+// How to debug nightwatch tests:
 // 1. Add logging, e.g.
 //   client.getLogTypes(function(result) {
-//    console.log('RESULT1', result);
+//    console.log(result);
 //  }).getLog('browser', function(result) {
-//      console.log('RESULT2', result);
+//      console.log(result);
 //  });
-// 2. Use firefox browser and pause the script, client.pause()
-// 3. Use screenshots
-
-
-//const beforeEach = client => {
-//  client
-//    .url(client.launch_url + url)
-//    .waitForElementVisible('body', 1000);
-//};
+// 2. Use real browser e.g. firefox browser (see nightwatch.json) and pause the script and pause the script: client.pause()
+// 3. Use screenshots, see nightwatch.json:
+//    "screenshots": {
+//      "enabled": true,
+//      "on_failure" : true,
+//      "on_error" : false,
+// 4. Use the Phantomjs REPL
 
 const displaysPage = client => {
+  // GIVEN, WHEN
   client.page.accounts().gotoAccounts();
+
+  // THEN
+  client.expect.element('#nordnet-react-app-light').to.be.present;
   client.end();
 };
 
 const displaysAccounts = client => {
-  // some sanity checks
+  // GIVEN
   const accounts = client.page.accounts();
+
+  // WHEN
   accounts.gotoAccounts();
-  accounts.login('stora', 'secret', 'sv-SE');
+  accounts.login();
   client.waitForElementVisible('.accounts-list', 5000);
+
+  // THEN
   client.expect.element('.accounts-list .alias').to.be.present;
   client.end();
 };
 
-//const displaysAccounts = client => {
-//  client.waitForElementVisible('select', 1000);
-//  client.waitForElementVisible('input', 1000);
-//  client.expect.element('#nordnet-react-app-light').to.be.present;
-//  client.expect.element('#login-container').to.be.present;
-//  client.assert.containsText('#login-container', 'Username');
-//  client.setValue('input[type=text]', 'stora');
-//  client.expect.element('button').to.be.present;
-//  client.click('select option[value="sv-SE"]');
-//  client.waitForElementVisible('select', 1000);
-//  client.click('button');
-//  client.waitForElementVisible('.accounts-list', 5000);
-//  client.expect.element('.accounts-list .alias').to.be.present;
-//  client.end();
-//};
-
 export default {
-//  'Page loads': displaysPage,
+  'Page loads': displaysPage,
   'Displays accounts': displaysAccounts,
 };
