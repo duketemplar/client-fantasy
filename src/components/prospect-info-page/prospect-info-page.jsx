8869bb9d6c5ea3016faa57fcb4c6c38063e9946b
Per Andersen <per.andersen@nordnet.se>
2016-01-22 15:23:53 +0100

    playing around with routes and react bootstrap

--- src/components/prospect-info-page/prospect-info-page.jsx
+++ src/components/prospect-info-page/prospect-info-page.jsx
@@ -1,49 +1,50 @@
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Button, Col, Row, Input   } from 'react-bootstrap';
 require('bootstrap/dist/css/bootstrap.css');
 import '../create-customer.sass';
 import ContactInfo from './contact-info';
 import RegulationData from './regulation-data';
+import { Link, browserHistory } from 'react-router';
 
 export default class ProspectInfoPage extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      showModal: true,
-      paneIndex: 0,
-      validForNextStep: false,
-    };
-
-    this.panes = [ContactInfo, RegulationData];
   }
 
   render() {
     return (
       <div className="create-customer">
-        <Modal show={ this.state.showModal }>
-          <Modal.Header>
-            <Modal.Title>Becoming a customer - Contact Info</Modal.Title>
-          </Modal.Header>
 
-          <Modal.Body>
-            { React.createElement(this.panes[this.state.paneIndex], { dataUpdatedCb: this.enableNextStep.bind(this) }) }
-          </Modal.Body>
+        <Col xs={12}>
+          <Row>
+            <h1>Becoming a customer - Contact Info</h1>
+          </Row>
 
-          <Modal.Footer>
-            <Button onClick={ this.close.bind(this) }>
-              Abort
-            </Button>
-            <Button onClick={ this.previous.bind(this) } disabled={ this.state.paneIndex === 0 }>
-              Previous Step
-            </Button>
-            <Button onClick={ this.next.bind(this) } disabled={ !this.state.validForNextStep } bsStyle="primary">
-              Next step
-            </Button>
-          </Modal.Footer>
-      </Modal>
+          <form onSubmit={ this.submitForm.bind(this) }>
+            <Col xs={4}>
+              <Row>
+                <Input name="firstName" type="text" label="First Name" placeholder="Marcus" />
+              </Row>
+              <Row>
+                <Input name="lastName" type="text" label="Last Name" placeholder="Berggren" />
+              </Row>
+              <Row>
+                <Input name="civicRegistrationNumber" type="text" label="Civic Registration Number" placeholder="yyyymmdd-xxxx" />
+              </Row>
+              <Row>
+                <Input name="submit" type="submit" />
+              </Row>
+            </Col>
+          </form>
+        </Col>
       </div>
     );
+  }
+
+  submitForm(e) {
+    console.log("yolo");
+    e.preventDefault();
+    this.props.history.go('regulation-info');
   }
 
   close() {
