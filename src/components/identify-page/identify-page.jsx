0ddf4b30f60d56c4e74942ae3e366e4d679d2533
Per Andersen <per.andersen@nordnet.se>
2016-02-24 17:08:27 +0100

    added some styling to identify page

--- src/components/identify-page/identify-page.jsx
+++ src/components/identify-page/identify-page.jsx
@@ -1,9 +1,10 @@
+import './identify-page.scss';
+
 import React from 'react';
 import { Input, Button } from 'nordnet-ui-kit';
 import { Grid, Col, Row } from 'react-bem-grid';
 
 class IdentifyPage extends React.Component {
-
   handleSubmit() {
     this.context.router.push({
       pathname: '/register/contact-info',
@@ -13,8 +14,8 @@ class IdentifyPage extends React.Component {
   render() {
     return (
       <Grid className="identify-page">
-        <Row xsMiddle>
-          <Col xs="12">
+        <Row xsMiddle xsCenter>
+          <Col xs={ 6 }>
             <form onSubmit={ this.handleSubmit.bind(this) } >
               <Input type="text" label="National registration number" />
               <Button type="submit">
