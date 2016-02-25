854077f89fdcab04e777932a564fecc62c4f9694
macber <marcus.berggren@nordnet.se>
2016-02-25 10:38:04 +0100

    Adding post.

--- src/components/sign-page/sign-page.jsx
+++ src/components/sign-page/sign-page.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Button } from 'nordnet-ui-kit';
 import { Grid, Col, Row } from 'react-bem-grid';
+import nordnetAPI from 'nordnet-next-api';
 
 const docs = [
   { name: 'Agreement Banking', url: 'waiiit-for-it' },
@@ -14,15 +15,36 @@ export default class SignPage extends React.Component {
     super(props);
   }
 
+  postSign() {
+    const url = '/api/2/signicat/sign';
+    const headers = {};
+    const params = { signUrls: docs.map(doc => doc.url) };
+
+    headers.contentType = 'application/json; charset=utf-8';
+
+    nordnetAPI
+    .post(url, params, headers)
+      .then(({ data }) => {
+        if (data.status === 'SUCCESS') {
+          console.log('SUCCESS! ');
+        } else {
+          console.log('Prospect data is not valid! ', data.error);
+        }
+      })
+      .catch(() => {
+        throw Error(`Could not post to ${url}`);
+      });
+  }
+
   handleSign() {
-    console.log('Will now need to sign something....');
+    this.postSign();
   }
 
   renderDocuments() {
     return (
       <div>
         <h3>The following documents needs to be signed to complete the process</h3>
-        <ul>
+        <ol>
         { docs.map((doc, indx) => {
           return (
             <li key={ indx }>
@@ -30,7 +52,7 @@ export default class SignPage extends React.Component {
             </li>
             );
         })}
-        </ul>
+        </ol>
       </div>
     );
   }
@@ -41,12 +63,14 @@ export default class SignPage extends React.Component {
       <div className="sign-page" style={ { width: '768px' }}>
         <Grid>
           <Row xsCenter>
-            <Col xs={12}>
+            <Row>
+              <Col xs={12}>
               { this.renderDocuments() }
-            </Col>
-            <Col xs={12}>
-              <Button primary onClick={ handleSign }>Sign</Button>
-            </Col>
+              </Col>
+              <Col xs={12}>
+                <Button primary onClick={ handleSign }>Sign</Button>
+              </Col>
+            </Row>
           </Row>
         </Grid>
       </div>
