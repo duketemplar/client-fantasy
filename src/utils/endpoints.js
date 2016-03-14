44b209cfb060019070beec557ebb4255d7dc70a2
macber <marcus.berggren@nordnet.se>
2016-03-14 11:13:13 +0100

    Removed host from nordnet api uri.

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,20 +1,5 @@
 /* jscs:disable maximumLineLength */
-function getNordnetAPIURI(hostname) {
-  const nordnetAPImap = {
-    local: '/api/2',
-    test: 'https://eservice-nnapi.test.nordnet.se:80/api/2',
-    ci: 'https://eservice-nnapi.ci.nordnet.se:80/api/2',
-  };
+const NORDNET_API_PATH = '/api/2';
 
-  const environment = encodeURI(hostname.split('.').slice(-3, -2).pop());
-
-  return nordnetAPImap[environment] ? nordnetAPImap[environment] : nordnetAPImap.local;
-}
-
-const nordnetAPIURI = getNordnetAPIURI(location.hostname);
-
-export default {
-  getNordnetAPIURI,
-  CUSTOMERS_PROSPECTS_URI: `${nordnetAPIURI}/customers/prospects`,
-  MANUAL_FLOW_OPEN_ISK_PATH: '/mux/web/nordnet/blikund.html?inhibit_nc=1&depa_typ=isk&agandeskap=private',
-};
+export const CUSTOMERS_PROSPECTS_PATH = `${NORDNET_API_PATH}/customers/prospects`;
+export const MANUAL_FLOW_OPEN_ISK_PATH = '/mux/web/nordnet/blikund.html?inhibit_nc=1&depa_typ=isk&agandeskap=private';
