e901b657f410919d26813995f28260643f6190e1
Per Andersen <per.andersen@nordnet.se>
2016-03-14 13:53:27 +0100

    lint fix

--- src/utils/validators/national-registration-number-validator.js
+++ src/utils/validators/national-registration-number-validator.js
@@ -1,23 +1,23 @@
 const seValidator = (value) => {
   return value.match(/^(\d{6,8})-?\d{4}$/);
-}
+};
 
 const dkValidator = (value) => {
-  return value.match(/^(\d{6,8})-?\d{4}$/);
-}
+  return value.match(/^(\d{6})-?\d{4}$/);
+};
 
 const fiValidator = (value) => {
-  return value.match(/^\d{6,8}-?\d{3}[0-9a-zA-Z?]/);
-}
+  return value.match(/^\d{6}-?\d{3}[0-9a-zA-Z?]/);
+};
 
 const noValidator = (value) => {
-  return value.match(/^(\d{6,8})-?\d{5}$/);
-}
+  return value.match(/^(\d{6})-?\d{5}$/);
+};
 
-const validators = {se: seValidator, dk: dkValidator, no: noValidator, fi: fiValidator };
+const validators = { se: seValidator, dk: dkValidator, no: noValidator, fi: fiValidator };
 
 const lengthValidator = (nationality, message, value) => {
-  if (value === undefined || nationality === undefined) {
+  if (value === undefined || value === '' || value === null || nationality === undefined) {
     return null;
   }
 
