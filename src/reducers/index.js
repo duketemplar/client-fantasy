951e9c4f56990f4f079fdf7f05e5febf04e9a467
Robin Sand <robin.sand@nordnet.se>
2016-03-29 15:48:34 +0200

    refactor reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import prospect from './prospect.reducer.js';
 import modal from './modal.reducer.js';
 import regulation from './regulation.reducer.js';
+import kyc from './kyc.reducer.js';
+import pep from './pep.reducer.js';
 import taxInfo from './tax-info.reducer.js';
 
 export default combineReducers({
   ...prospect,
   ...modal,
   ...regulation,
+  ...kyc,
+  ...pep,
   ...taxInfo,
 });
