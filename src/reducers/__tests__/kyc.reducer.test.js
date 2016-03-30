5abbcf4d3bdcace240700981a403c66756e906ac
Robin Sand <robin.sand@nordnet.se>
2016-03-30 12:41:21 +0200

    merge fix

--- src/reducers/__tests__/kyc.reducer.test.js
+++ src/reducers/__tests__/kyc.reducer.test.js
@@ -5,7 +5,7 @@ import { changeKyc } from '../../actions';
 
 describe('kyc.reducer', () => {
   it('should initialize kyc', () => {
-    expect(kycReducers.kyc(undefined, { type: '@@init' })).to.eql(new Kyc());
+    expect(reducers.kyc(undefined, { type: '@@init' })).to.eql(new Kyc());
   });
 
   it('should update only changed kyc fields', () => {
@@ -17,7 +17,7 @@ describe('kyc.reducer', () => {
       employment: true,
     });
 
-    expect(kycReducers.kyc(initialState, action)).to.eql(new Kyc({
+    expect(reducers.kyc(initialState, action)).to.eql(new Kyc({
       employment: true,
       income: 'cash money',
     }));
@@ -34,7 +34,7 @@ describe('kyc.reducer', () => {
       yearlyDeposits: null,
       politicallyExposedPerson: null,
     });
-    expect(kycReducers.kycValidations(initialState, action)).to.eql({
+    expect(reducers.kycValidations(initialState, action)).to.eql({
       taxableOutsideJurisdiction: 'This question needs to be answered.',
       USCitizen: 'This question needs to be answered.',
       employment: 'This question needs to be answered.',
