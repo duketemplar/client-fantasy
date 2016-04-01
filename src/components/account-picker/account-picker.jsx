f25ae4eaea9d72660cc1af382a9e453223517e2f
Fredrik Gustafsson <Fredrik.Gustafsson@nordnet.se>
2016-04-01 09:22:45 +0200

    Formatting fix for text column

--- src/components/account-picker/account-picker.jsx
+++ src/components/account-picker/account-picker.jsx
@@ -12,23 +12,23 @@ export default class AccountPicker extends React.Component {
             <div className="account__name">
               <div className="account__name__text">ISK</div>
             </div>
-            <Col className="account__description" xs={ 8 }>
+            <Col className="account__description" xs={ 9 }>
               <div className="account__description__text">
                 As a new customer an investment savings account will be opened for you.
               </div>
               <div className="account__description__text">
                 Once you are logged in, you can easily open more accounts and other account types.
               </div>
-            </Col>
-            <div className="account__usp">
-              <img src="../assets/images/shape.svg" /> <div className="account__usp__text">Our most popular account</div>
+              <div className="account__usp">
+                <img src="../assets/images/shape.svg" /> <div className="account__usp__text">Our most popular account</div>
+                </div>
+                <div className="account__usp">
+                <img src="../assets/images/shape.svg" /> <div className="account__usp__text">No account fees</div>
               </div>
-              <div className="account__usp">
-              <img src="../assets/images/shape.svg" /> <div className="account__usp__text">No account fees</div>
-            </div>
-              <div className="account__usp">
-              <img src="../assets/images/shape.svg" /> <div className="account__usp__text">No declaration</div>
-            </div>
+                <div className="account__usp">
+                <img src="../assets/images/shape.svg" /> <div className="account__usp__text">No declaration</div>
+              </div>
+            </Col>
           </Col>
         </Row>
       </Grid>
