51382ca30fc7cbeaeb125f08c90ceb6803c4f247
macber <marcus.berggren@nordnet.se>
2016-04-05 10:13:19 +0200

    Info modal.

---
+++ src/components/info-modal/__test__/info-modal.test.js
@@ -0,0 +1,82 @@
+/* global expect */
+import InfoModal from '../info-modal';
+import React from 'react';
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
+
+describe('info modal', () => {
+  let wrapper;
+  let sandbox;
+  let onAcceptSpy;
+  let onCancleSpy;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+
+    onAcceptSpy = sinon.spy(() => false);
+    onCancleSpy = sinon.spy(() => false);
+
+    const props = {
+      show: true,
+      content: null,
+      onAccept: onAcceptSpy,
+      onCancle: onCancleSpy,
+    };
+
+    wrapper = shallow(<InfoModal {...props} />);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+    wrapper = null;
+  });
+
+  it('is hidden when property show is false', () => {
+    wrapper.setProps({ show: false });
+    expect(wrapper.type()).to.be.equal(null);
+  });
+
+  it('is shown when property show is true', () => {
+    wrapper.setProps({ show: true });
+    expect(wrapper.type()).not.to.be.equal(null);
+  });
+
+  it('displays the content', () => {
+    const content = (<div><h2>header</h2><p>body</p></div>);
+    wrapper.setProps({
+      show: true,
+      content,
+    });
+
+    const component = wrapper.find('.info-modal__content');
+
+    expect(component.contains(content)).to.equal(true);
+  });
+
+  it('perform the accept callback when accept is clicked', () => {
+    const accept = wrapper.find('.info-modal__action').childAt(0);
+
+    wrapper.setProps({ show: true });
+    accept.simulate('click');
+
+    expect(onAcceptSpy.calledOnce).to.equal(true);
+  });
+
+  it('trigger cancle callback when cancle is triggered', () => {
+    const cancle = wrapper.find('.info-modal__action').childAt(1);
+
+    wrapper.setProps({ show: true });
+    cancle.simulate('click');
+
+    expect(onCancleSpy.calledOnce).to.equal(true);
+  });
+
+  it('trigger cancle callback when clicked outside modal', () => {
+    const outsideModal = wrapper.find('.info-modal__bkg');
+
+    wrapper.setProps({ show: true });
+    outsideModal.simulate('click');
+
+    expect(onCancleSpy.calledOnce).to.equal(true);
+  });
+});
--- src/components/info-modal/info-modal.jsx
+++ src/components/info-modal/info-modal.jsx
@@ -1,70 +1,71 @@
 import './info-modal.scss';
 import React from 'react';
 import { Button } from 'nordnet-ui-kit';
-import { MANUAL_FLOW_OPEN_ISK_PATH } from '../../utils/endpoints';
-import { connect } from 'react-redux';
-import { toggleModal } from '../../actions';
 
 class InfoModal extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  accept() {
-    window.location = location.origin + MANUAL_FLOW_OPEN_ISK_PATH;
-  }
-
-  hide() {
-    this.props.dispatch(toggleModal(false));
-  }
-
   render() {
-    if (!this.props.showModal) {
+    const { show, content, onAccept, onCancle } = this.props;
+
+    if (!show) {
       return null;
     }
 
-    const accept = this.accept;
-    const closeInfoBox = this.hide.bind(this, false);
-
     return (
       <div>
-        <div className="compliance__bkg" onClick={ closeInfoBox }></div>
-        <div className="compliance__info">
-          <h1>
-            We need to know more about you!
-          </h1>
-          <p className="compliance__info--text">
-            With the grounds that you have answered <b>Yes</b> to one of the questions
-            below, we ask that you apply via the extended manual process so that
-            we can get some more detailed information about you.
-          </p>
-          <ul>
-            <li className="compliance__list--crs">
-              Are you a US citizen, tax or declaration obliged in other countries than Sweden?
-            </li>
-            <li className="compliance__list--pep">
-              Have you, or have you ever had: a high political or government office position
-              or are a close family member or an employee of a person in the above position?
-            </li>
-          </ul>
-          <div className="compliance__info--buttons">
-            <Button primary onClick={ accept }>Continue</Button>
-            <Button secondary onClick={ closeInfoBox }>Cancel</Button>
-          </div>
+        <div className="compliance__bkg info-modal__bkg" onClick={ onCancle }></div>
+        <div className="compliance__info info-modal__content">
+          { content }
+        </div>
+        <div className="compliance__info--buttons info-modal__action">
+          <Button primary onClick={ onAccept }>Continue</Button>
+          <Button secondary onClick={ onCancle }>Cancel</Button>
         </div>
       </div>
     );
   }
 }
 
-function select(state) {
-  return {
-    showModal: state.showModal,
-  }
+/*
+accept() {
+window.location = location.origin + MANUAL_FLOW_OPEN_ISK_PATH;
 }
+<div>
+  <div className="compliance__bkg info-modal__bkg" onClick={ closeInfoBox }></div>
+  <div className="compliance__info info-modal__content">
+    <h1>
+      We need to know more about you!
+    </h1>
+    <p className="compliance__info--text">
+      With the grounds that you have answered <b>Yes</b> to one of the questions
+      below, we ask that you apply via the extended manual process so that
+      we can get some more detailed information about you.
+    </p>
+    <ul>
+      <li className="compliance__list--crs">
+        Are you a US citizen, tax or declaration obliged in other countries than Sweden?
+      </li>
+      <li className="compliance__list--pep">
+        Have you, or have you ever had: a high political or government office position
+        or are a close family member or an employee of a person in the above position?
+      </li>
+    </ul>
+    <div className="compliance__info--buttons info-modal__action">
+      <Button primary onClick={ accept }>Continue</Button>
+      <Button secondary onClick={ closeInfoBox }>Cancel</Button>
+    </div>
+  </div>
+</div>
+*/
 
 InfoModal.propTypes = {
-  showInfo: React.PropTypes.bool.isRequired,
+  show: React.PropTypes.bool.isRequired,
+  content: React.PropTypes.element.isRequired,
+  onAccept: React.PropTypes.func.isRequired,
+  onCancle: React.PropTypes.func.isRequired,
 };
 
-export default connect(select)(InfoModal)
+export default InfoModal;
