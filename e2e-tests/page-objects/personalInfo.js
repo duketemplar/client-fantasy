e885a49b1faeb4c6c81d74c018d05b789bc0d780
Fredrik Gustafsson <Fredrik.Gustafsson@nordnet.se>
2016-04-11 10:34:33 +0200

    Made phone and email tests more specific

--- e2e-tests/page-objects/personalInfo.js
+++ e2e-tests/page-objects/personalInfo.js
@@ -2,13 +2,13 @@ module.exports = (client) => {
   return {
     insertPhone() {
       return client
-      .waitForElementVisible('#webapp-customer-registration', 1500)
+      .waitForElementVisible('.prospect__input_phone', 1500)
       .setValue('#phone-number', '0762343684');
     },
 
     insertEmail() {
       return client
-      .waitForElementVisible('#webapp-customer-registration', 1500)
+      .waitForElementVisible('.prospect__input_email', 1500)
       .setValue('#e-mail', 'konkar@nordnet.se');
     },
   };
