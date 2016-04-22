193e5b2f18ac015d1449ba40ce635517fd2416dd
macber <marcus.berggren@nordnet.se>
2016-04-22 14:27:26 +0200

    Sign section styling.

--- src/components/sign-page/sign-page.jsx
+++ src/components/sign-page/sign-page.jsx
@@ -1,11 +1,13 @@
 /* jscs:disable maximumLineLength */
 /* eslint-disable max-len */
 import React from 'react';
-
 import { Button, Checkbox } from 'nordnet-ui-kit';
-import { Grid, Col, Row } from 'react-bem-grid';
+import { Grid, Row, Col } from 'react-bem-grid';
 import { connect } from 'react-redux';
 import { toggleAcceptedAggreements, freezeProspect } from '../../actions';
+import './sign-page.scss';
+import UspBackground from './open-landscape-gazing.png';
+import DocumentIcon from './document-icon.svg';
 
 class SignPage extends React.Component {
   constructor(props) {
@@ -14,49 +16,24 @@ class SignPage extends React.Component {
     this.state = {
       isSigning: false,
     };
+
+    this.handleSign = this.handleSign.bind(this);
+    this.handleAcceptTermsAndConditions = this.handleAcceptTermsAndConditions.bind(this);
+    this.getTermsAndCondition = this.getTermsAndCondition.bind(this);
   }
 
-  getContitionText() {
+  getTermsAndCondition() {
+    const termsAndConditionLink = <a href="#" onClick={ this.alert('This is the terms and conditions...') }>terms and conditions</a>;
     return (
-      <span>
-        Many cryptographers would agree that, had it not been for the transistor, the refinement of Lamport clocks that would allow forfurther study into 32 bit architectures might never have occurred.
-        In fact, few theorists would disagree with the exploration of web browsers. Continuing with this rationale, to put this in perspective, consider the fact that acclaimed statisticians regularly
-        use I/O automata to answer this quagmire. Unfortunately, Lamport clocks alone is not able to fulfill the need for decentralized configurations. We introduce an analysis of DNS, which we call ChoroidCOD.
-        The usual methods for the exploration of evolutionary programming do not apply in this area. We emphasize that ChoroidCOD can be synthesized to prevent the refinement of Scheme. Thus, we describe new
-        interactive communication (ChoroidCOD), disconfirming that Moore's Law can be made interposable, distributed, and ambimorphic. Motivated by these observations, lossless symmetries and write-back
-        caches have been extensively developed by hackers worldwide. We emphasize that our heuristic turns the interposable configurations sledgehammer into a scalpel. In the opinions of many, the basic
-        tenet of this solution is the construction of digital-to-analog converters. Our methodology can be improved to learn consistent hashing. Nevertheless, the transistor might not be the panacea that
-        analysts expected. This work presents two advances above prior work. Primarily, we consider how link-level acknowledgements [28] can be applied to the construction of multi-processors. On a similar
-        note, we disprove that context-free grammar can be made empathic, permutable, and encrypted [2,3,39,28]. The rest of this paper is organized as follows. For starters, we motivate the need for symmetric
-        encryption. Continuing with this rationale, we place our work in context with the prior work in this area. Furthermore, to overcome this issue, we describe a methodology for neural networks (ChoroidCOD),
-        which we use to prove that suffix trees and architecture can agree to accomplish this goal. Finally, we conclude.
-      </span>
+      <div>
+        I have read and accepted the current { termsAndConditionLink } and I do solemnly swear that above filled is correct.
+        In case of changes i will update my info under my settings when i have logged in.
+      </div>
     );
   }
 
-  postSign() {
-    /*
-    const url = '/api/2/signicat/sign';
-    const headers = {};
-    const params = { signUrls: 'https://nordnet-agreements.pdf.nordnet.se/customer/retail' };
-
-    headers.contentType = 'application/json; charset=utf-8';
-
-    const _this = this;
-    nordnetAPI
-    .post(url, params, headers)
-    .then(({ data }) => {
-      if (data.status === 'SIGNED') {
-        console.log('Singning completed, id: ', data.signID); // eslint-disable-line no-console
-        _this.context.router.push('/');
-      } else {
-        console.log('Prospect data is not valid! ', data.error); // eslint-disable-line no-console
-      }
-    })
-    .catch((error) => {
-      throw Error(`Could not post to ${url}: ${error.message}`);
-    });
-    */
+  handleAcceptTermsAndConditions(event) {
+    this.props.dispatch(toggleAcceptedAggreements(event.target.checked));
   }
 
   handleSign() {
@@ -67,49 +44,68 @@ class SignPage extends React.Component {
     this.props.dispatch(freezeProspect());
   }
 
-  handleCancel() {
-    this.context.router.push('/');
-  }
-
-  handleAcceptTermsAndConditions(event) {
-    this.props.dispatch(toggleAcceptedAggreements(event.target.checked));
+  alert(msg) {
+    return (event) => {
+      event.preventDefault();
+      alert(msg); //eslint-disable-line no-alert
+    };
   }
 
   render() {
-    const handleSign = this.handleSign.bind(this);
-    const handleCancel = this.handleCancel.bind(this);
-    const handleAcceptTermsAndConditions = this.handleAcceptTermsAndConditions.bind(this);
-
     return (
-      <Grid className="sign__page">
-        <h1>
-          Terms & conditions
-        </h1>
-        <p>
-          These contain information about the account, risk of securities and guarantees
-          you as a customer of our bank. Please read and accept before you sign.
-        </p>
-        <Grid className="sign__component">
-          <Row xsCenter>
-            <Col xs={ 12 }>
+      <Grid className="sign">
+        <Row>
+          <Col xs={ 12 }>
+            <div className="sign__header">
+              <h1>
+                Terms & conditions
+              </h1>
               <p>
-                { this.getContitionText() }
+                These contain information about the account, risk of securities and guarantees
+                you as a customer of our bank. Please read and accept before you sign.
               </p>
-              <Checkbox
-                id="sign-read-agreement-and-conditions"
-                className="checkbox__terms-and-conditions"
-                label="I have read and accepted the terms and conditions above."
-                checked={ this.props.sign.acceptedAgreements }
-                onClick={ handleAcceptTermsAndConditions }
-                disabled={ this.state.isSigning }
-              />
-            </Col>
-          </Row>
-          <Row xsEnd>
-            <Button secondary onClick={ handleCancel } >Cancel</Button>
-            <Button id="sign-do-signing" onClick={ handleSign } disabled={ !this.props.sign.acceptedAgreements || this.state.isSigning } primary>Sign</Button>
-          </Row>
-        </Grid>
+            </div>
+            <div className="usp-sign" style={ { backgroundImage: `url(${UspBackground})` } }>
+              <div className="usp-sign__container">
+                <h3>Comfortable and secure for you</h3>
+                <ul>
+                  <li>No account charges</li>
+                  <li>When trading shares the courtage starts at 1BTC</li>
+                  <li>When trading founds you only pay for the found companys yearly fee</li>
+                  <li>Your accounts is secured by the state</li>
+                </ul>
+              </div>
+            </div>
+            <div className="terms-and-conditions">
+                <div>
+                  <Button
+                    className="terms-and-conditions__fetcher"
+                    secondary
+                    onClick={ this.alert('This will show some pdfs...') }
+                    style={ { backgroundImage: `url(${DocumentIcon})` } }
+                  >
+                    Agreements and Conditions (PDF)
+                  </Button>
+                  <p className="terms-and-conditions__agreements-and-conditions-info">
+                    All agreements and conditions will also be sent to your email.
+                  </p>
+                </div>
+                <div className="horizontal-rule"></div>
+                <div className="terms-and-conditions__accept">
+                  <Checkbox
+                    id="sign-read-agreement-and-conditions"
+                    label={ this.getTermsAndCondition() }
+                    checked={ this.props.sign.acceptedAgreements }
+                    onClick={ this.handleAcceptTermsAndConditions }
+                    disabled={ this.state.isSigning }
+                  />
+                </div>
+            </div>
+            <div className="sign__action">
+              <Button id="sign-do-signing" onClick={ this.handleSign } disabled={ !this.props.sign.acceptedAgreements || this.state.isSigning } primary>Sign</Button>
+            </div>
+          </Col>
+        </Row>
       </Grid>
     );
   }
@@ -118,11 +114,6 @@ class SignPage extends React.Component {
 SignPage.propTypes = {
   dispatch: React.PropTypes.func,
   sign: React.PropTypes.object,
-};
-
-// TODO: Remove router from context or else stub it in tests
-SignPage.contextTypes = {
-  router: React.PropTypes.func.isRequired,
 };
 
 function select(state) {
